f35b6f6b74c47cd6b7ce2649766bc88ac8db0593
xuemeng1987 <85297739+xuemeng1987@users.noreply.github.com>
2024-10-31 20:20:58 +0800

    updeta index.html and scrip.js uplode image

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+function updateImage(newImagePath) {
+    const updateImageElement = document.querySelector(".update-image");
+    if (updateImageElement) {
+        updateImageElement.src = newImagePath;
+    } else {
+        console.error("找不到更新示例圖片的元素！");
+    }
+}
+
 function loadMoreUpdates() {
     alert("目前沒有更多更新內容！");
 }
+
+function changeToGif() {
+    updateImage('images/update-image.gif');
+}
+
+document.querySelector("button").addEventListener("click", changeToGif);
