d9912374947c936d3b2d51b5344f479d663a8b04
xuemeng1987 <85297739+xuemeng1987@users.noreply.github.com>
2024-11-01 16:39:19 +0800

    Update script.js
    fix bug

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
-function updateImage(newImagePath) {
-    const updateImageElement = document.querySelector(".update-image");
-    if (updateImageElement) {
-        updateImageElement.src = newImagePath;
-    } else {
-        console.error("找不到更新示例圖片的元素！");
-    }
-}
-
-function loadMoreUpdates() {
-    alert("目前沒有更多更新內容！");
-}
-
-function changeToGif() {
-    updateImage('images/update-image.gif');
-}
-
-document.querySelector("button").addEventListener("click", changeToGif);
-
 document.addEventListener("DOMContentLoaded", function () {
     const loader = document.getElementById("loader");
     loader.style.display = "none";
+
+    function updateImage(newImagePath) {
+        const updateImageElement = document.querySelector(".update-image");
+        if (updateImageElement) {
+            updateImageElement.src = newImagePath;
+        } else {
+            console.error("找不到更新示例圖片的元素！");
+        }
+    }
+
+    function loadMoreUpdates() {
+        alert("目前沒有更多更新內容！");
+    }
+
+    function changeToGif() {
+        updateImage('images/update-image.gif');
+    }
+
+    const gifButton = document.querySelector("button");
+    if (gifButton) {
+        gifButton.addEventListener("click", changeToGif);
+    } else {
+        console.error("找不到觸發更換圖片的按鈕！");
+    }
 });
